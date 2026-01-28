fc93f5061c8f303e64645821a437f2180128117f
@giszhc <giszhc@163.com>
2026-01-28 19:55:35 +0800

    thumbnail v0.2.2

--- Formula/thumbnail.rb
+++ Formula/thumbnail.rb
@@ -1,8 +1,8 @@
 class Thumbnail < Formula
   desc "Batch generate image thumbnails using libvips"
   homepage "https://github.com/giszhc/vips-thumbnail"
-  url "https://github.com/giszhc/vips-thumbnail/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "5709e7d2e073d052c1650859c047779a4048e801d3423b6ba04b3301fafe33b4"
+  url "https://github.com/giszhc/vips-thumbnail/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "3ccf00d1207993f5e68481670d33a6c69be0c5374fd035d338e0764fd44adc6e"
   license "MIT"
 
   depends_on "vips"
